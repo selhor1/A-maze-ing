8babe7bef2d68abebcf2c9d07e44584a4c1af577
Mohamed Amine Asselgui <masselgu@e1r6p14.1337.ma>
2026-03-04 02:22:56 +0000

    commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mazegen",
+    version="1.0.0",
+    packages=find_packages(),
+    description="Maze generator and solver",
+    author="massegu & selhor",
+    python_requires=">=3.8",
+)
